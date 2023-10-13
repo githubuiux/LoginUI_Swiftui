acae10ae6e49bb2686fc9fbe20000636f3a16e3c
githubuiux <uiuxista@gmail.com>
2023-10-13 11:19:49 +0300

    Initial Commit

---
+++ LoginUI_Swiftui/LoginUI_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUI_SwiftuiApp.swift
+//  LoginUI_Swiftui
+//
+//  Created by Levent on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginUI_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
